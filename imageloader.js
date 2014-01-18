d9845d2ca44ae26fbf6c16041267a38f66e293c2
ruanyl <ruanyu1@gmail.com>
2014-01-18 16:56:16 +0200

    initial

---
+++ imageloader.js
@@ -0,0 +1,79 @@
+var _imgReady = (function () {
+    var list = [], intervalId = null,
+
+    // 用来执行队列
+    tick = function () {
+        var i = 0;
+        for (; i < list.length; i++) {
+            list[i].end ? list.splice(i--, 1) : list[i]();
+        };
+        !list.length && stop();
+    },
+
+    // 停止所有定时器队列
+    stop = function () {
+        clearInterval(intervalId);
+        intervalId = null;
+    };
+
+    return function (url, ready, load, error) {
+        var onready, width, height, newWidth, newHeight,
+            img = new Image();
+
+        img.src = url;
+
+        // If cached, return data directly
+        if (img.complete) {
+            ready.call(img);
+            load && load.call(img);
+            return;
+        };
+
+        width = img.width;
+        height = img.height;
+
+        // loaded error
+        img.onerror = function () {
+            error && error.call(img);
+            onready.end = true;
+            img = img.onload = img.onerror = null;
+        };
+
+        // image size ready
+        onready = function () {
+            newWidth = img.width;
+            newHeight = img.height;
+            if (newWidth !== width || newHeight !== height ||
+                // if image loaded at other place with available size
+                newWidth * newHeight > 1024
+            ) {
+                ready.call(img);
+                onready.end = true;
+            };
+        };
+        onready();
+
+        // loaded completely
+        img.onload = function () {
+            // onload在定时器时间差范围内可能比onready快
+            // 这里进行检查并保证onready优先执行
+            !onready.end && onready();
+
+            load && load.call(img);
+
+            // IE gif动画会循环执行onload，置空onload即可
+            img = img.onload = img.onerror = null;
+        };
+
+        // 加入队列中定期执行
+        if (!onready.end) {
+            list.push(onready);
+            // 无论何时只允许出现一个定时器，减少浏览器性能损耗
+            if (intervalId === null) intervalId = setInterval(tick, 40);
+        };
+    };
+})();
+
+_imgReady("http://stwww.surfingmagazine.com/wp-content/blogs.dir/1/files/2012/06/girl-in-just-a-shirt-4.jpg", function() {
+            alert(this.width);
+        });
